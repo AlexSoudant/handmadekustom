77c1bb59ec56719afa199e49936208c7de7125da
Alex Soudant <alex.soudant@capgemini.com>
2021-12-06 23:26:37 +0100

    Updates

---
+++ cb1608f2-bf7405d4cb8cb4db4451.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-bf7405d4cb8cb4db4451.js.LICENSE.txt */
+"use strict";(self.webpackChunkkustom_made=self.webpackChunkkustom_made||[]).push([[523],{8014:function(c,e,n){n.d(e,{mTx:function(){return u}});var u={prefix:"fas",iconName:"chevron-up",icon:[448,512,[],"f077","M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"]}}}]);
+//# sourceMappingURL=cb1608f2-bf7405d4cb8cb4db4451.js.map
